bc35c2316a2dde777d7b61b7b005f9a47f66a7d1
Trang <tmh327@drexel.edu>
2019-04-02 00:38:22 -0400

    Chap 8 first

---
+++ w3/Ch8e1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+fruit = 'Orange'
+for char in fruit[::-1]:
+    print (char)
---
+++ w3/Ch8e2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+prefix = 'JKLMNOPQ'
+suffix = 'ack'
+for letter in prefix:
+    if letter in "OQ":
+        print (prefix + 'u' + suffix)
+    print(prefix+suffix)
+
