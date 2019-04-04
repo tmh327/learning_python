442abe5d30b7cf5c8ab326c343bfc087e123dd31
Trang <tmh327@drexel.edu>
2019-04-04 18:18:41 -0400

    Not solved.

--- w4/Ch8e6.py
+++ w4/Ch8e6.py
@@ -1,1 +1,13 @@
-
+#!/usr/bin/env python3
+def find(word,letter,start):
+    while start < len(word):
+        if word[start] == letter:
+            return start
+        start += 1
+    return -1
+def count(string,letter,findcount):
+    start = 0 
+    while start < len(string):
+        if string[count] == letter:
+            count += 1
+    return 
