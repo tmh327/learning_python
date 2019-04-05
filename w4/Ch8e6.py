211525be9fa9ca02d4d49164ac75f05c0346973c
Trang <tmh327@drexel.edu>
2019-04-05 15:28:16 -0400

    push exercise 6

--- w4/Ch8e6.py
+++ w4/Ch8e6.py
@@ -2,12 +2,18 @@
 def find(word,letter,start):
     while start < len(word):
         if word[start] == letter:
-            return start
-        start += 1
-    return -1
-def count(string,letter,findcount):
-    start = 0 
-    while start < len(string):
-        if string[count] == letter:
-            count += 1
-    return 
+            return start    
+        else:
+            start += 1
+    return -1     
+
+
+
+def count(string,letter):
+    start, count = 0,0   
+    while find(string,letter,start) != -1:
+        count += 1
+        start = find(string,letter,start)+1
+    return count
+        
+print(count('banana','a')) 
